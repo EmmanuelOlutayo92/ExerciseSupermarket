0729e021469576833122a634f1e00e0dfd3a6130
EmmanuelOlutayo92 <emmanuelolutayo92@gmail.com>
2020-12-11 05:03:49 +0000

    Testing 2

--- SupermarketCheckout/Properties/AssemblyInfo.cs
+++ SupermarketCheckout/Properties/AssemblyInfo.cs
@@ -7,7 +7,7 @@ using System.Runtime.InteropServices;
 // associated with an assembly.
 [assembly: AssemblyTitle("SupermarketCheckout")]
 [assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyConfiguration("yes")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("SupermarketCheckout")]
 [assembly: AssemblyCopyright("Copyright ©  2020")]
